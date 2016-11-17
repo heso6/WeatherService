9c10a7a3ecfbd218257b8ebde3d693c3948f2ec3
Rafał Dudziak <dudziak.rafal@gmail.com>
2016-11-17 17:45:32 +0100

    added json and commons dependencis

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,19 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 575));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+        try {
+            JSONObject  json = new JSONObject(IOUtils.toString(new URL("https://api.apixu.com/v1/current.json?key=d578c483ed8b494bad8170534161511&q=Warsaw"), Charset.forName("UTF-8")));
+            System.out.println(json);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+
     }
 }
