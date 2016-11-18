a5d45aaff18ebedc04913d31bd7210f376633602
Rafał Dudziak <dudziak.rafal@gmail.com>
2016-11-18 13:31:37 +0100

    add new option and onAction

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import sample.model.Weather;
@@ -28,22 +32,41 @@ public class Controller implements Initializable {
     @FXML
     private  Label cityLabel;
 
-    private Weather currentWeather;
+    @FXML
+    private Button sendButton;
 
-    public void initialize(URL location, ResourceBundle resource){
-        setValuesComponentsFromServer();
+    @FXML
+    private TextField cityTextField;
+
+    @FXML
+    public void onEnter(ActionEvent actionEvent){
+        setValuesComponentsFromServer(cityTextField.getText());
     }
 
-    private void setValuesComponentsFromServer() {
+
+    private Weather currentWeather;
+
+
+    private void setValuesComponentsFromServer(String myCity) {
         WeatherServiceJsonOrg weatherServiceJsonOrg = new WeatherServiceJsonOrg("d578c483ed8b494bad8170534161511");
-        currentWeather = weatherServiceJsonOrg.getWeatherFromUrl("http://api.apixu.com/v1/current.json","Warsaw");
+        String city = myCity !=null ? myCity : "Warsaw";
+        currentWeather = weatherServiceJsonOrg.getWeatherFromUrl("http://api.apixu.com/v1/current.json",city);
         dateLabel.setText(currentWeather.getLastUpdated());
         cityLabel.setText(currentWeather.getLocation());
         temperatureLabel.setText(String.valueOf(currentWeather.getFeelsLikeTemperatureInCelcius()));
         textConditionLabel.setText(currentWeather.getWeatherCondition().getText());
-        Image image = new Image("http"+currentWeather.getWeatherCondition().getIconUrl(), true);
+        Image image = new Image("http:"+currentWeather.getWeatherCondition().getIconUrl(), true);
         iconImageView.setImage(image);
     }
 
 
+    public void initialize(URL location, ResourceBundle resources) {
+        setValuesComponentsFromServer("Warsaw");
+
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        setValuesComponentsFromServer(cityTextField.getText());
+                    }
+                });
+    }
 }
